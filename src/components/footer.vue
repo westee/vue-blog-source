64e2deadd3fdd5ca966036c75c1fb53fb7a7d523
dflk <904885508@qq.com>
2023-03-19 15:02:52 +0800

    init

---
+++ src/components/footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer>
+    <p>© westee</p>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  align-self: end;
+  background-color: #d7d7d7;
+  color: #666;
+  font-size: 13px;
+  padding: 10px;
+  text-align: center;
+}
+
+</style>
